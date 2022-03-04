63dd5f4c940a066f900e083d3c342a11b570d7f5
mochenlyq <1019854044@qq.com>
2022-03-04 22:18:30 +0800

    配置数据库

---
+++ app/src/main/java/com/example/ouku/db/City.java
@@ -0,0 +1,46 @@
+package com.example.ouku.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    private int provinceId;
+}
